0b71c27136f377e9a6b17930ab2929cb35cb59f2
ahmad <ahmadsaeed8081@gmail.com>
2023-12-15 01:31:52 +0500

    login update 1

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -46,7 +46,6 @@ const Login = ({setuser}) => {
         {
           setuser(response.data[0].userAddress,response.data[0]);
           // setuser("0x7D8d2d6BCDC1e586F94F4ca77a4BC92E3f6CD738",response.data[0]);
-          alert("hello")
           dispatch(setUserToken(true));
           navigate("dashboard/home");
 
